c0cbe324d1830d2f514172d08f07f3e9cb28ab42
spuniun <spuniun@gmail.com>
2026-05-19 11:52:21 -0400

    update mounts from plexdrive to rclone

--- mnt_plex.sh
+++ mnt_plex.sh
@@ -3,8 +3,7 @@ export ENCFS6_CONFIG='/home/plex/encfs6.xml'
 echo "Enter encryption passphrase"
 read -s PASSPHRASE
 
-#sudo systemctl start plexdrive.service
-#sudo mount -t nfs4 -o sec=krb5p,clientaddr=173.251.23.199 totalmonkey.cpam.tv:/home/spuniun /mnt/nfs/totalmonkey/
+sudo systemctl start rclone-gdrive.service
 
 echo $PASSPHRASE | encfs -S /home/plex/.local-sorted /home/plex/local-sorted -o allow_other -o umask='002'
 if [ $? -ne 0 ]; then
@@ -22,12 +21,4 @@ unionfs -o cow -o allow_other /home/plex/local-sorted=RW:/home/plex/gdrive-sorte
 
 docker compose -f /home/plex/cpam/arrs/docker-compose.yml up -d
 sleep 30
-#sudo systemctl start plexmediaserver.service
-#cd /opt/telegram
-#sudo -u spuniun screen -dm python start.py
-#cd /opt/Mellow
-#screen -dm npm start
-#cd /opt/MusicBot
-#screen -dm nodejs index.js
-#cd /opt/PlexRecs
-#sudo -u spuniun screen -dm python PlexRecsGeneral.py
+sudo systemctl start plexmediaserver.service
--- umnt_plex.sh
+++ umnt_plex.sh
@@ -4,9 +4,7 @@ sudo systemctl stop plexmediaserver.service
 docker compose -f /home/plex/cpam/arrs/docker-compose.yml down
 
 fusermount -u /home/plex/sorted/
-sleep 15
 fusermount -u /home/plex/gdrive-sorted
 fusermount -u /home/plex/local-sorted
 
-#sudo systemctl stop plexdrive.service
-#sudo umount /mnt/nfs/totalmonkey
+sudo systemctl stop rclone-gdrive.service
