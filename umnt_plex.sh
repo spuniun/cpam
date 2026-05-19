79f601bde768b5a95d412f8aad8dfb824a9a2bf2
spuniun <spuniun@gmail.com>
2026-05-19 11:09:15 -0400

    update paths of docker compose

--- umnt_plex.sh
+++ umnt_plex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo systemctl stop plexmediaserver.service
 
-docker compose -f /home/plex/cpam-arrs/docker-compose.yml down
+docker compose -f /home/plex/cpam/arrs/docker-compose.yml down
 
 fusermount -u /home/plex/sorted/
 sleep 15
@@ -9,4 +9,4 @@ fusermount -u /home/plex/gdrive-sorted
 fusermount -u /home/plex/local-sorted
 
 #sudo systemctl stop plexdrive.service
-sudo umount /mnt/nfs/totalmonkey
+#sudo umount /mnt/nfs/totalmonkey
